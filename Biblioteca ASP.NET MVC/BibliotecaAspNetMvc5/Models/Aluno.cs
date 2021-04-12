ddbaaee7c63cccf92903f1c3c17baa2482af7e66
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-04-12 19:30:13 -0300

    Biblioteca Validação de Modelos com DataAnnotations.

--- Biblioteca ASP.NET MVC/BibliotecaAspNetMvc5/Models/Aluno.cs
+++ Biblioteca ASP.NET MVC/BibliotecaAspNetMvc5/Models/Aluno.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,32 @@ namespace BibliotecaAspNetMvc5.Models
 {
     public class Aluno
     {
+        // DataAnnotations: são as validações dos dados que são feitas na model, através de um processo chamado: ModelBind.
+
+        // criando as propriedades
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string Nome { get; set; }
+
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é requerido!")]
+        public string CPF { get; set; }
+
+        public DateTime DataMatricula { get; set; }
+
+        public bool Ativo { get; set; }
+
+        //[Required(ErrorMessage = "O campo {0} é requerido!")]
+        //public string Senha { get; set; }
+
+        //[Compare(otherProperty:"Senha", ErrorMessage = "As senhas informadas não conferem")]
+        //[Required(ErrorMessage = "O campo {0} é requerido!")]
+        //public string SenhaConfirmacao { get; set; }
     }
 }
