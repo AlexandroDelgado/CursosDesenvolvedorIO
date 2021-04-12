296385d3ec02a4925a6392adecf7ea43a1be2e05
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-04-12 18:10:15 -0300

    Aula: Validação de Modelos

---
+++ 01 - Iniciando com ASP.NET MVC 5/TesteMVC5/Controllers/AlunoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteMVC5.Models;
+
+namespace TesteMVC5.Controllers
+{
+    // a classe está herdadno um controller
+    public class AlunoController : Controller
+    {
+        // cria a rota para navegação em url que simulara o recebimento de uma instância "aluno" para o método "Novo".
+        [Route(template:"Novo-Aluno")]
+
+        // A ActionResult está simulando a entrada de um novo aluno
+        // isto está sendo feito por não ter sido emprementado a view ainda.
+        public ActionResult Novo(Aluno aluno)
+        {
+            // simula a criação da instância de um aluno
+            aluno = new Aluno
+            {
+                // popula os dados do aluno para teste
+                Id = 1,
+                Nome = "Eduardo", // passe o nome vazio
+                CPF = "12345678912",
+                DataMatricula = DateTime.Now,
+                Email = "edu@edu.com", // troque a @ por 2
+                Ativo = true
+            };
+
+            // passa o objeto modelo(aluno) para a action Index.
+            return RedirectToAction("Index", aluno);
+        }
+
+        // Essa action está sendo criada para validar a passagem do aluno
+        public ActionResult Index(Aluno aluno)
+        {
+            // caso o modelo esteja correto, retorna o objeto aluno para a view informando que está correto.
+            if (!ModelState.IsValid) return View(aluno);
+
+            // Devolve o objeto aluno com erro
+            return View(aluno);
+        }
+
+    }
+}
---
+++ 01 - Iniciando com ASP.NET MVC 5/TesteMVC5/Models/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TesteMVC5.Models
+{
+    public class Aluno
+    {
+        // DataAnnotations: são as validações dos dados que podem ser feitas pela view, Controllers e etc.
+
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string Nome { get; set; }
+       
+        [DisplayName("E-mail")]
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        [EmailAddress(ErrorMessage = "E-mail em formato inválido")]
+        public string Email { get; set; }
+       
+        [Required(ErrorMessage = "O campo {0} é requerido")]
+        public string CPF { get; set; }
+        
+        public DateTime DataMatricula { get; set; }
+        
+        public bool Ativo { get; set; }
+
+        //[Required(ErrorMessage = "O campo {0} é requerido")]
+        //public string Senha { get; set; }
+
+        //[Compare(otherProperty:"Senha", ErrorMessage = "As senha informadas não conferem")]
+        //[Required(ErrorMessage = "O campo {0} é requerido")]
+        //public string SenhaConfirmacao { get; set; }
+    }
+}
