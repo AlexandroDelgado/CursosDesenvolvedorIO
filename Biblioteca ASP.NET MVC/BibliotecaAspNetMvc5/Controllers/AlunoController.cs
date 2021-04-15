a26746b45a9915aa4bf26a3dedc82dce55c719cb
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-04-15 13:05:01 -0300

    Operações no banco de dados com EntityFramework

--- Biblioteca ASP.NET MVC/BibliotecaAspNetMvc5/Controllers/AlunoController.cs
+++ Biblioteca ASP.NET MVC/BibliotecaAspNetMvc5/Controllers/AlunoController.cs
@@ -1,6 +1,8 @@
-﻿using BibliotecaAspNetMvc5.Models;
+﻿using BibliotecaAspNetMvc5.Data;
+using BibliotecaAspNetMvc5.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +12,9 @@ namespace BibliotecaAspNetMvc5.Controllers
     // a classe está herdando um controller
     public class AlunoController : Controller
     {
+        // cria o contexto para salvar no banco
+        private readonly AppDbContext context = new AppDbContext();
+
         /*
             [HttpGet] Declara o tipo do verbo. Quando retornarmos a view de forma vazia o correto é usar o GET e POST quando preenchido.
             [Route(template: "Novo-Aluno")] Cria a rota para navegação em url.
@@ -31,9 +36,18 @@ namespace BibliotecaAspNetMvc5.Controllers
         // A ActionResult verifica se retorna o objeto aluno como verdadeiro ou falso para a View 
         public ActionResult NovoAluno(Aluno aluno) 
         {
-            // caso o modelo esteja correto, retorna o objeto aluno para a view informando que está correto.
-            if (!ModelState.IsValid) 
+            // caso o modelo esteja correto.
+            if (ModelState.IsValid) 
             {
+                // Atribui a data da matricula
+                aluno.DataMatricula = DateTime.Now;
+
+                // Adiciona o contexto na memória
+                context.Alunos.Add(aluno);
+
+                // salva o aluno no banco
+                context.SaveChanges();
+
                 // Retorna a instância do objeto para a View como verdeiro
                 return View(aluno);
             }
@@ -42,6 +56,69 @@ namespace BibliotecaAspNetMvc5.Controllers
                 // retorna a instância do objeto para a view com falso
                 return View(aluno);
             }
+        }
+
+        [HttpGet]
+        [Route(template: "Obter-Alunos")]
+        // Exibe uma lista com os alunos
+        public ActionResult ObterAlunos()
+        {
+            // cria uma variavel com a lista de alunos
+            var alunos = context.Alunos.ToList();
+
+            // Retorna o primeiro aluno da lista
+            return View("NovoAluno", model: alunos.FirstOrDefault());
+        }
+
+        [HttpGet]
+        [Route("Editar-Aluno")]
+        // edita o aluno
+        public ActionResult EditarAluno()
+        {
+            // seleciona o primeiro aluno através do nome
+            var aluno = context.Alunos.FirstOrDefault(a => a.Nome == "Andreia Medeiros Ramos");
+
+            // atribui o novo nome para o aluno
+            aluno.Nome = "Andréia Delgado";
+
+            // cria uma variavel para ser salva no banco
+            var entry = context.Entry(aluno);
+
+            // adiciona o aluno ao contexto existente
+            context.Set<Aluno>().Attach(aluno);
+
+            // Modifica o aluno no contexto;
+            entry.State = EntityState.Modified;
+
+            // Salva o contexto
+            context.SaveChanges();
+
+            // Poderia enviar o objeto, porém esta obtendo o mesmo novamente
+            var alunoNovo = context.Alunos.Find(aluno.Id);
+
+            // retorna a view com o aluno novo
+            return View("NovoAluno", model: alunoNovo);
+        }
+
+        [HttpGet]
+        [Route("Excluir-Aluno")]
+        // remove o aluno da base
+        public ActionResult ExcluirAluno()
+        {
+            // Cria uma variavel com o aluno para exclusão
+            var aluno = context.Alunos.FirstOrDefault(a => a.Nome == "Eduardo");
+
+            // Remove o aluno atravé do seu objeto
+            context.Alunos.Remove(aluno);
+
+            // salva o contexto na base
+            context.SaveChanges();
+
+            // Recebe a nova lista de alunos
+            var alunos = context.Alunos.ToList();
+
+            // retorna o primeiro aluno da lista
+            return View("NovoAluno", model: alunos.FirstOrDefault());
         }
     }
 }
