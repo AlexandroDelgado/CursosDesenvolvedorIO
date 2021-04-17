324abc0a99e2b0228a05e15dc315020ba38484b0
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-04-17 17:45:26 -0300

    Escrevendo as entidades de negócio.

--- MeusProdutos/src/DevIO.Business/Models/Fornecedores/Endereco.cs
+++ MeusProdutos/src/DevIO.Business/Models/Fornecedores/Endereco.cs
@@ -5,6 +5,16 @@ namespace DevIO.Business.Models.Fornecedores
     // Essa classe também é uma entidade pois está herdando de entidade.
     public class Endereco : Entity
     {
+        // Propriedades
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; } // Criar um objeto chamado Estado que possua, o nome a sigla, a coleção de cidades, para como uma melhoria remover as propriedades Cidade.
 
+        /* Entity Framework - Relacionamento */
+        public Fornecedor Fornecedor { get; set; } // Utilizado aqui por que o EF não deixa utilizar o Id do fornecedor
     }
 }
--- MeusProdutos/src/DevIO.Business/Models/Produtos/Produto.cs
+++ MeusProdutos/src/DevIO.Business/Models/Produtos/Produto.cs
@@ -1,9 +1,22 @@
-﻿using DevIO.Business.Core.Models;
+﻿using System;
+using DevIO.Business.Core.Models;
+using DevIO.Business.Models.Fornecedores;
 
 namespace DevIO.Business.Models.Produtos
 {
     // A classe produto está herdando a classe pai Entity (Tranformando ela em uma entidade, já que está herdando de uma entidade).
     public class Produto : Entity
     {
+        // Propriedades
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* Entity Framework - Relacionamento */
+        public Fornecedor Fornecedor { get; set; } // Utilizado aqui por que o EF não deixa utilizar o Id do fornecedor
     }
 }
