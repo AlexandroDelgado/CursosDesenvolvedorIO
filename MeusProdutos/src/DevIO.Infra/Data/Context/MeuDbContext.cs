93ffc0e98e66b3852c2e91f89a336b7a0f147a04
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-04-18 19:10:00 -0300

    Correções da base de dados

--- MeusProdutos/src/DevIO.Infra/Data/Context/MeuDbContext.cs
+++ MeusProdutos/src/DevIO.Infra/Data/Context/MeuDbContext.cs
@@ -2,6 +2,7 @@
 using DevIO.Business.Models.Produtos;
 using DevIO.Infra.Data.Mappings;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace DevIO.Infra.Data.Context
 {
@@ -10,7 +11,11 @@ namespace DevIO.Infra.Data.Context
     {
         // O contrutor herda da base a connecition string para conexão com o banco
         public MeuDbContext() : base(nameOrConnectionString: "DefaultConnection")
-        { }
+        {
+            // Desabilita as configurações que geral alta carga de processamento da aplicação
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
 
         // Mapeamento DbSet
         public DbSet<Produto> Produtos { get; set; }
@@ -20,10 +25,23 @@ namespace DevIO.Infra.Data.Context
         // Cria os modelos no banco de dados
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            // Adiciona as configurações dos mapeamentos
+            // Convenções
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); // Não permite que o Entity crie tabelas no plural automaticamente
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>(); // Não permite que o Entity exclua registros de um para muitos automaticamente
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>(); // Não permite que o Entity exclua registros de muitos para muitos automaticamente
+
+            modelBuilder.Properties<string>() // Configurando os campos do tipo string
+                .Configure(p =>
+                    p.HasColumnType("varchar") // Informa que todos os campos do tipo string serão varchar
+                    .HasMaxLength(100)); // do tamanho máximo de 100 caracteres.
+
+            // Adiciona as configurações dos mapeamentos, que irão sobreescrever qualquer configuração acima.
             modelBuilder.Configurations.Add(new FornecedorConfig());
             modelBuilder.Configurations.Add(new EnderecoConfig());
             modelBuilder.Configurations.Add(new ProdutoConfig());
+
+            // Executa todas as configurações do banco
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
