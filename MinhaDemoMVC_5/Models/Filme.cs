1d2fa48188fd6f0bdebc7299681b9f508297cffb
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-05-02 11:54:30 -0300

    Trabalhando com Models

---
+++ MinhaDemoMVC_5/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MinhaDemoMVC_5.Models
+{
+    public class Filme
+    {
+        // Propriedades
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Genero { get; set; }
+        public decimal Valor { get; set; }
+        public int Avaliacao { get; set; }
+    }
+}
