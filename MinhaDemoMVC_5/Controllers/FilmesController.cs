772ee24e1668692dc8281ef29fa405b638e2af13
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-05-03 22:29:16 -0300

    Trabalhanco com formulários.

---
+++ MinhaDemoMVC_5/Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMVC_2.Controllers.Filmes
+{
+    public class FilmesController : Controller
+    {
+        // Informa o verbo que entrega para o client
+        [HttpGet]
+        // Método de controle
+        public IActionResult Adicionar()
+        {
+            // Retorna a view de mesmo nome
+            return View();
+        }
+    }
+}
--- MinhaDemoMVC_5/Models/Filme.cs
+++ MinhaDemoMVC_5/Models/Filme.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace MinhaDemoMVC_5.Models
 {
