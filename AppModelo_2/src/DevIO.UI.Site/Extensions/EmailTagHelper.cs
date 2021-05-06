92d3cbf1134f7230405ab1795ed818ff7b06b874
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-05-06 20:17:04 -0300

    Email Tag Helper - Inicio

---
+++ AppModelo_2/src/DevIO.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.UI.Site.Extensions
+{
+    public class EmailTagHelper
+    {
+        //
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+
+        
+    }
+
+}
