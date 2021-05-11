d2e9987910d61844e414a998cfc55be026782a7e
Alexandro Delgado <alexandrodelgado@hotmail.com>
2021-05-11 13:02:18 -0300

    Dominando o ASP.NET MVC Core Injetando dependências no MVC

--- AppModelo_2/src/DevIO.UI.Site/Controllers/HomeController.cs
+++ AppModelo_2/src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc; /*Referência da controller*/
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc; /*Referência da controller*/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,22 @@ namespace DevIO.UI.Site.Controllers
     // Herda da classe Controller
     public class HomeController : Controller
     {
-        // Action para retornar a index
-        public IActionResult Index()
+        // Propriedade privada para a injeção de dependência.
+        private readonly IPedidoRepository _pedidoRepository;
+
+        // Construtor que recebe uma injeção de dependência dentro do controller
+        public HomeController(IPedidoRepository pedidoRepository)
         {
+            // Informamos que a proprieade privada recebe o conteúdo do tipo "pedidoRepository que informamos no construtor.
+            _pedidoRepository = pedidoRepository;
+        }
+
+        // Action para retornar a index
+        public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
+        {
+            // Acessa a propriedade Id, do método, atravé da injeção de dependência.
+            var Id = _pedidoRepository.ObterPedido().Id;
+
             return View();
         }
     }
